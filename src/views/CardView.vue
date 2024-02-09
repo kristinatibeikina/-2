acb0e927014aa285f0ab2a3bac001690b9f2e9e7
Kristina <kristina.tibeykina04@mail.ru>
2024-02-10 00:39:27 +0700

    Увеличение товаров в корзине

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,24 +1,31 @@
 <template>
   <h1 class="catalog">Корзина </h1>
   <div class="row">
-    <div class="col-md-3" v-for="product in cart">
+    <div class="col-md-3" v-for="product in cart ">
       <div class="container">
         <div class="card">
-          <div class="face face1">
+          <div class="face face1 face3 f">
             <div class="content">
               <h3>{{ product.name }}</h3>
             </div>
           </div>
-          <div class="face face2">
-            <div class="content">
+          <div class="face face2 content-bloc face3">
+            <div class="content ">
               <p>{{product.description}}</p>
               <h4>${{ product.price}}</h4>
-              <button type="submit" @click="deleteCard(product)">Удалить</button><br>
-              <button type="submit" @click="arrangeCard">Оформить</button>
-            </div>
+              <div>{{countCard}}</div>
+              <button @click="addCount" class="btn-count btn-count-right">+</button>
+              <button @click="deleteCount" class="btn-count">-</button>
+              <div class="button-panel delete-btn">
+                <input type="submit" class="button button-card" @click="deleteCard(product)" value="Удалить">
+              </div>
+              <div class="button-panel arrange-btn">
+                <input type="submit" class="button button-card" @click="arrangeCard"  value="Оформить">
+              </div>
           </div>
         </div>
       </div>
+    </div>
     </div>
   </div>
 </template>
@@ -28,12 +35,14 @@ export default {
     return{
       url: 'https://jurapro.bhuser.ru/api-shop',
       cart: [],
+      countCard:1
     }
   },
   created() {
     this.viewCard()
   },
   methods: {
+
     async deleteCard(product){ //Удаление карточек товаров
       const userToken = localStorage.getItem('userToken');
       const url = `https://jurapro.bhuser.ru/api-shop/cart/${product.id}`;
@@ -87,13 +96,53 @@ export default {
         console.log('Ошибка при добавление товара')
       }
 
-},
+    },
+    addCount() {
+      this.countCard++;
+      this.saveLocalStorage();
+
+    },
+    deleteCount() {
+      if (this.countCard > 1) {
+        this.countCard--;
+        this.saveLocalStorage();
+      }
+    },
+    saveLocalStorage() {
+      localStorage.setItem('quantity', JSON.stringify(this.countCard));
+    }
   }
 }
 
 </script>
 <style>
+.delete-btn{
+
+}
+.arrange-btn{
+  margin-top: 20px;
+}
+.container .card .face3{
+  width: 300px;
+  height: 400px;
+  transition: 0.5s;
+}
+.container .card .face.f{
+  transform: translateY(300px);
+  transition: 0.5s;
+}
+.btn-count{
+  width: 30px;
+  height: 30px;
+  background: #49b65f;
+  color: #fafafa;
+  border: none;
+}
 .catalog{
   color: #ff00a9;
 }
+.btn-count-right{
+  margin-right: 20px;
+}
+
 </style>
